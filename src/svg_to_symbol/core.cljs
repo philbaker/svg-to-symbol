d8761d798bb51fa0fcaa9d84ab1c58b40cd2b61f
philbaker <phil@philbaker.me>
2023-03-05 12:44:47 +0000

    Add basic error handling

--- src/svg_to_symbol/core.cljs
+++ src/svg_to_symbol/core.cljs
@@ -2,16 +2,36 @@
   (:require [clojure.string :as str]
             ["node-html-parser" :as html :refer [parse]]
             ["fs" :as fs]
+            ["path" :as path]
             ["svgo$default" :refer [optimize]]))
 
-(def cmd-line-args (not-empty (js->clj (.slice js/process.argv 2))))
+; Command line arguments
+(def cmd-line-args (js->clj (.slice js/process.argv 2)))
 (def file-name (first cmd-line-args))
 (def symbol-id (second cmd-line-args))
 
+; Error handling
+(when-not
+  (= 2 (count cmd-line-args))
+  (println "svg-to-symbol expects two arguments: file-name and symbol-id")
+  (js/process.exit 1))
+
+(when-not
+  (fs/existsSync file-name)
+  (println file-name "does not exist")
+  (js/process.exit 1))
+
+(when-not
+  (= (path/extname file-name) ".svg")
+  (println "file must have the extension .svg")
+  (js/process.exit 1))
+
+; SVG transformation
 (defn svg->symbol [file-name id] 
   (let [file (str (fs/readFileSync file-name))
         optimized-svg (:data (js->clj (optimize file) :keywordize-keys true))  
         path-tag (str (.querySelector (parse optimized-svg) "path"))]
     (str "<symbol id=\"" id "\" viewbox=\"0 0 24 24\">" path-tag "</symbol>")))
 
+; Output
 (println (svg->symbol file-name symbol-id))
