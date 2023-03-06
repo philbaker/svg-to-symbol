efc21ea7eccc86413c56da26304edb6ddd6598b3
philbaker <phil@philbaker.me>
2023-03-06 22:44:23 +0000

    Add test and make main function pure

--- src/svg_to_symbol/core.cljs
+++ src/svg_to_symbol/core.cljs
@@ -9,6 +9,7 @@
 (def cmd-line-args (js->clj (.slice js/process.argv 2)))
 (def file-name (first cmd-line-args))
 (def symbol-id (second cmd-line-args))
+(def file-contents (str (fs/readFileSync file-name)))
 
 ; Error handling
 (when-not
@@ -27,13 +28,12 @@
   (js/process.exit 1))
 
 ; SVG transformation
-(defn svg->symbol [file-name id] 
-  (let [file (str (fs/readFileSync file-name))
-        view-box (.getAttribute (.querySelector (parse file) "svg") "viewBox")
+(defn svg->symbol [file id] 
+  (let [view-box (.getAttribute (.querySelector (parse file) "svg") "viewBox")
         optimized-svg (:data (js->clj (optimize file) :keywordize-keys true))  
         path-tag (str (.querySelector (parse optimized-svg) "path"))
         path-tag-self-close (clojure.string/replace path-tag #"></path>" "/>")]
     (str "<symbol id=\"" id "\" viewBox=\"" view-box "\">" path-tag-self-close "</symbol>")))
 
 ; Output
-(println (svg->symbol file-name symbol-id))
+(println (svg->symbol file-contents symbol-id))
